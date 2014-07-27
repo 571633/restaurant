e7618de654393609c9799dd10ef79ff7ffb6b8d4
571633 <hechuanjun@msn.com>
2014-07-27 17:01:45 -0400

    modified:   DM_redirect.js

--- js/DM_redirect.js
+++ js/DM_redirect.js
@@ -8,7 +8,7 @@ function DM_redirect(MobileURL, Home){
 		var CurrentUrl = location.href;
 		var noredirect = document.location.search;
 		if (noredirect.indexOf("no_redirect=true") < 0){
-			if ((navigator.userAgent.match(/^[^\[]*(iPhone|iPod|BlackBerry|Android.*Mobile|BB10.*Mobile|webOS|Windows CE|IEMobile|Opera Mini|Opera Mobi|HTC|LG-|LGE|SAMSUNG|Samsung|SEC-SGH|Symbian|Nokia|PlayStation|PLAYSTATION|Nintendo DSi).*$/im)) ) {
+			if ((navigator.userAgent.match(/^[^\[]*(iPhone|iPod|iPad|BlackBerry|Android.*Mobile|BB10.*Mobile|webOS|Windows CE|IEMobile|Opera Mini|Opera Mobi|HTC|LG-|LGE|SAMSUNG|Samsung|SEC-SGH|Symbian|Nokia|PlayStation|PLAYSTATION|Nintendo DSi).*$/im)) ) {
 				
 				if(Home){
 					location.replace(MobileURL);
